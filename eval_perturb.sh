661f85c4329b6a2f53bc7d1b079a5c27cabecb03
BoYuanVisionary <yuanbo096@gmail.com>
2025-02-20 20:31:57 -0500

    remove openai api

--- eval_perturb.sh
+++ eval_perturb.sh
@@ -1,6 +1,3 @@
-# os.environ["OPENAI_API_KEY"] = "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"
-export OPENAI_API_KEY="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"
-
 python eval_perturb.py --model "simplescaling/s1-32B" --run_times 3 --task "AIME24" --perturbation "add_word"
 python eval_perturb.py --model "simplescaling/s1-32B" --run_times 3 --task "AIME24" --perturbation "remove_word"
 
